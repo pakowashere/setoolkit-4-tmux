ef1741e82bc01e464dce71f5f0cab02327b033e5
ivam3 <ivam3.bh@gmail.com>
2017-11-26 12:59:52 -0600

    eliminar

---
+++ SET_4_Tmux.sh
@@ -0,0 +1,203 @@
+#!/data/data/com.termux/files/usr/bash
+#Created on 11/01/2017 by Ivam3
+
+clear
+echo "***Ivam3 is calling Cinderella back"
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back ."
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . ."
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . . ."
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . . / "
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . . - "
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . . \ "
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . . | "
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . . / "
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . . -"
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . . \ "
+sleep 1
+clear
+echo "***Ivam3 is calling Cinderella back . . @ "
+echo
+apt update && apt upgrade -y
+apt install -y figlet util-linux wget tar git termux-exec
+clear
+
+#Special thanks
+setterm -foreground cyan
+	echo "####################################";
+setterm -foreground magenta 
+	echo "THANKS TO MY MASTER CINDERELLA QEPD";
+setterm -foreground cyan
+	echo "####################################";
+
+#BANNER
+          echo
+          echo "============================";
+          echo ".___                  _______";
+          echo "|   |__ ______   ___  \_____ \ ";
+          echo "|   \ \/ /\__ \ /   \    (__ <,";
+          echo "|   |\   / __ \| Y Y \ \      \ ";
+          echo "|___| \_/ (____|__|_| /______ /";
+          echo "              \/    \/      \/ ";
+          echo "====== By ___ Cinderella ======";
+          echo ""
+          echo "   -{ Termux - Setoolkit } "
+          echo "     -{ Coded by Ivam3 } "
+          echo "      -{ Intalation } "
+          echo
+          echo "Choose your language"
+	  echo "1) English"
+	  echo "2) Espanol"
+          echo
+          read -p "R= " answer
+          clear
+
+case $answer in
+	1)
+
+#Disclaimer
+setterm -foreground white
+		echo "Using And Installing This Tools That Mean You're Ready For :"
+		echo
+		echo "[1] Use At Your Own Risk"
+		echo "[2] No Warranty"
+		echo "[3] If someone violates the law with this tool the risk is borne by the user"
+		echo
+setterm -foreground blue
+		echo "-----------------------------------------------------------------------------------"
+		echo "                   Are You Sure Want To Install SETOOLKIT ?"
+		echo "-----------------------------------------------------------------------------------"
+		echo
+setterm -foreground white
+		echo "                            Press Enter if you Agree"
+		echo "                                       or"
+		echo "                         Press CTRL + C if you Disagree"
+		read ENTER
+		clear
+
+#Start to install
+setterm -foreground magenta
+		echo "***Cinderella will start to install Setoolkit"
+		sleep 1
+		clear
+		echo "***Cinderella will start to install Setoolkit ."
+		sleep 1
+		clear
+		echo "***Cinderella will start to install Setoolkit . ."
+		sleep 1
+		clear
+		echo "***Cinderella will start to install Setoolkit . . ."
+		sleep 1
+setterm -foreground white
+		git clone https://github.com/ivam3/setoolkit-4-tmux
+		cd setoolkit-4-tmux
+		echo
+setterm -foreground magenta
+		echo "Cinderella is Downloading and Extracting . . ."
+setterm -foreground white
+		apt install ./setoolkit_7.7.4_all.deb
+		echo
+		cd ../rm -r setoolkit-4-tmux
+		echo
+		
+setterm -foreground yellow                                                                                                  echo " >>>>>> www.youtube.com/c/Ivam3byCinderella <<<<<<"
+setterm -foreground red
+	      echo " ____  _   _ ____ ____   ____ ____  ___ ____  _____"
+              echo "/ ___|| | | | __ ) ___| / ___|  _ \|_ _| __ )| ____"
+	      echo "\___ \| | | |  _ \___ \| |   | |_) || ||  _ \|  _|"
+	      echo " ___) | |_| | |_) |__) | |___|  _ < | || |_) | |___"
+	      echo "|____/ \___/|____/____/ \____|_| \_\___|____/|_____|"
+setterm -foreground yellow
+              echo " >>>>>>>>>>>>>>>>> LIKE && SHARE <<<<<<<<<<<<<<<<<"
+setterm -foreground blue
+              echo
+              echo " ### CONTACTAME EN :=> fb.me/Ivam3byCinderella ###"
+	      sleep 3
+	      echo "
+	      "
+setterm -foreground green
+        echo " NOW YOU CAN LAUNCH SETOOLKIT BY JUST EXECUTE THE COMMAND :=> setoolkit"
+	;;
+
+	2)
+setterm -foreground white
+                echo "Al Utilizar E Instalar Esta Herramienta Significa Que Estas Listo Para :"
+		echo ""
+		echo "[1] Usarla Bajo Tu Propio Riesgo"
+		echo "[2] No Hay Garantias"
+		echo "[3] Si Se Viola La Ley Con Esta Herramienta La Responsiva Sera Del Usuario"
+		echo
+setterm -foreground blue
+		echo "-----------------------------------------------------------------------------------"
+		echo "                Estas Seguro De Querer Instalar SETOOLKIT ?"
+		echo "-----------------------------------------------------------------------------------"
+		echo
+setterm -foreground white
+		echo "                      Presiona ENTER si estas de acuerdo"
+		echo "                                     ó"
+		echo "                   Presiona CTRL + C si no estas de acuerdo"
+		read ENTER
+		clear
+setterm -foreground magenta
+		echo "***Cinderella Iniciara La Instalació De Setoolkit"
+		sleep 1
+                clear
+                echo "***Cinderella Iniciara La Instalació De Setoolkit ."
+                sleep 1
+		clear
+		echo "***Cinderella Iniciara La Instalació De Setoolkit . ."
+                sleep 1
+                clear
+                echo "***Cinderella Iniciara La Instalació De Setoolkit . . ."
+                sleep 1
+setterm -foreground white
+                git clone https://github.com/ivam3/setoolkit-4-tmux
+                cd setoolkit-4-tmux
+                echo
+setterm -foreground magenta
+                echo "Cinderella Esta Descargando y  Extrayendo . . ."
+setterm -foreground white
+                apt install ./setoolkit_7.7.4_all.deb
+                echo
+                cd ../rm -r setoolkit-4-tmux
+                echo
+
+setterm -foreground yellow	
+	echo " >>>>>> www.youtube.com/c/Ivam3byCinderella <<<<<<"
+setterm -foreground red
+	echo " ____  _   _ ____ ____   ____ ____  ___ ____  _____"
+	echo "/ ___|| | | | __ ) ___| / ___|  _ \|_ _| __ )| ____|"
+	echo "\___ \| | | |  _ \___ \| |   | |_) || ||  _ \|  _|"
+	echo " ___) | |_| | |_) |__) | |___|  _ < | || |_) | |___"
+	echo "|____/ \___/|____/____/ \____|_| \_\___|____/|_____|"
+setterm -foreground yellow
+	echo " >>>>>>>>>>>>>>>>> LIKE && SHARE <<<<<<<<<<<<<<<<<"
+setterm -foreground blue
+	echo
+	echo " ### CONTACTAME EN :=> fb.me/Ivam3byCinderella ###"
+	sleep 3
+	echo "
+
+	"
+setterm -foreground green
+	echo "LISTO YA PUEDES CORRER SETOOLKIT SOLO EJECUTANDO EL COMANDO :=> setoolkit "
+	esac
